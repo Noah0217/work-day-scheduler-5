c26cfbad43eac547724d589e1f97654e071d4f3b
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-02 00:34:42 -0600

    Add files via upload

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,36 @@
-//
+// Format For Date In Header
+var time = moment();
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+//Using local storage to save and retrieve data on refresh
+$(".time").click(function() {
+var time = $(this).attr("id");
+
+var schedule = localStorage.getItem(time);
+
+if (schedule !==null) {
+  
+  $(this).children(".schedule").val(schedule);
+}
+
+});
+
+
+//This saves the text when refreshed
+ openPlanner();
+ $(".save").on("click", function () {
+   var time = $(this).parent().attr("id");
+
+   var schedule = $(this).siblings(".schedule").val();
+
+   localStorage.setItem(time, schedule);
+
+ })
+
+
+
+ 
+
+
+
+
